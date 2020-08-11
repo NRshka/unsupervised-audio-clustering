4331c945cee77ada6ac405f370d94cb01dc4316d
Александр Макаров <makarov.alxr@yandex.ru>
2020-08-11 16:33:47 +0700

    tensorboard writer

--- dclust/model/trainer.py
+++ dclust/model/trainer.py
@@ -25,7 +25,7 @@ class Trainer:
         return torch.sqrt(torch.mean((y_hat-y)**2))
 
 
-    def training_step(self, dataloader = None):
+    def training_step(self, dataloader = None, writer = None):
         '''Produce a training step over dataset and return average loss'''
         if not (self.dataloader or dataloader):
             raise ValueError('You should provide dataloader')
@@ -38,29 +38,36 @@ class Trainer:
             target = x.flip(1) # reverse along time
 
             recon = self.model(x)
-            
+
             loss = Trainer.compute_loss(recon, target)
-            
+
             #print(f'\r{ind+1}/{len(train_dataloader)} | {loss.item()}', end='')
             avg_loss += loss.item()
+            if writer:
+                writer.add_scalar('RMSE', loss.item(), ind)
             self.optimizer.zero_grad()
             loss.backward()
             self.optimizer.step()
-        
+
         avg_loss /= len(train_dataloader)
+
         return avg_loss
 
-    def train(self, count_epochs, dataloader=None):
-        '''Iterate over epochs and return losses history'''
+    def train(self, count_epochs, dataloader = None, writer = None):
+        '''Iterate over epochs and return losses history.
+        Writer is instance of torch.utils.tensorboard writer'''
         losses_history = []
         for epoch in range(count_epochs):
-            loss_on_epoch = self.training_step(dataloader)
+            loss_on_epoch = self.training_step(dataloader, writer)
             losses_history.append(loss_on_epoch)
+
+            if writer:
+                writer.add_scalar('Average loss on epoch', loss_on_epoch, epoch)
             print(f'{epoch + 1} / {count_epochs} | Loss: {loss_on_epoch}')
 
         return losses_history
 
-    
+
     def extract_features(self, dataloader):
         self.model = self.model.eval()
 
